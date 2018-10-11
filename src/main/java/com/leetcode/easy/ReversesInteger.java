70462562ef6a5fab5b1f1e95d33a6c9049eea962
wangxinhui <wangxinhuileft@163.com>
2018-10-11 20:14:07 +0800

    reverses

---
+++ src/main/java/com/leetcode/easy/ReversesInteger.java
@@ -0,0 +1,12 @@
+package com.leetcode.easy;
+
+/**
+ * @Author: by simple on 2018/10/11.
+ */
+public class ReversesInteger {
+
+    public static void main(String[] args) {
+        int a = 123;
+
+    }
+}
--- src/main/java/com/leetcode/easy/TwoSum.java
+++ src/main/java/com/leetcode/easy/TwoSum.java
@@ -6,11 +6,12 @@ import java.util.Map;
 public class TwoSum {
 
     public static void main(String[] args) {
-        int[] res = {1, 3, 5, 78, 9, 6, 4};
+        int[] res = {1, 3, 5, 78, 7, 6, 4,9};
         int[] s = resultNums(res,10);
-        System.out.println(s);
-
+        System.out.println(s[0] + "------" +  s[1]);
     }
+
+
     public static int[] resultNums(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         int[] res = new int[2];
@@ -19,6 +20,7 @@ public class TwoSum {
             if (map.containsKey(target - nums[i])){
                 res[0] = i;
                 res[1] = map.get(target-nums[i]);
+                break;
             }
             map.put(nums[i],i);
         }
