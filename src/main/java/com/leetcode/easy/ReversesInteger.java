041f044631fa8ab7f52856446f89f70e3c32138a
wangxinhui <wangxinhuileft@163.com>
2018-10-12 08:18:38 +0800

    integer反转

--- src/main/java/com/leetcode/easy/ReversesInteger.java
+++ src/main/java/com/leetcode/easy/ReversesInteger.java
@@ -6,7 +6,17 @@ package com.leetcode.easy;
 public class ReversesInteger {
 
     public static void main(String[] args) {
-        int a = 123;
-
+        int a = 1534236469;
+        int res = 0;
+        while (a!=0){
+            int temp = res*10 + a%10;
+            a /=10;
+            if (temp/10!=res){
+                res = 0;
+                break;
+            }
+            res= temp;
+        }
+        System.out.println(res);
     }
 }
