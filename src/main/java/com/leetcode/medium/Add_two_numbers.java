4e627d069b77714849f4e948a664101123fcfbee
wangxinhui <wangxinhuileft@163.com>
2018-10-29 19:13:42 +0800

    add two numbers

---
+++ src/main/java/com/leetcode/medium/Add_two_numbers.java
@@ -0,0 +1,8 @@
+package com.leetcode.medium;
+
+/**
+ * @Author: by simple on 2018/10/29.
+ */
+public class Add_two_numbers {
+
+}
---
+++ src/main/java/com/leetcode/medium/Node.java
@@ -0,0 +1,23 @@
+package com.leetcode.medium;
+
+import java.util.LinkedList;
+
+/**
+ * @Author: by simple on 2018/10/29.
+ */
+public class Node {
+    private Node next;
+    private int data;
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    public void displayLink(){
+        System.out.println("{"+data+"}");
+    }
+
+}
+class LinkList{
+
+}
