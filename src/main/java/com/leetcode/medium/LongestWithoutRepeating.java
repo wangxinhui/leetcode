d7ddc9638b6e644cd86ed46c81e40cc5e069e9fb
wangxinhui <wangxinhuileft@163.com>
2019-07-30 18:01:35 +0800

    LongestWithoutRepeating

--- src/main/java/com/leetcode/medium/LongestWithoutRepeating.java
+++ src/main/java/com/leetcode/medium/LongestWithoutRepeating.java
@@ -9,34 +9,34 @@ import java.util.Set;
 public class LongestWithoutRepeating {
 
     public static void main(String[] args) {
+        System.out.println(getLongestStr("abcabcadec"));
 
     }
 
-    public static String getLongestStr(String str) {
+    public static int getLongestStr(String str) {
         int length = str.length();
         int ans = 0;
 
         for (int i = 0; i < length; i++) {
             for (int j = i+1; j <= length; j++) {
-
+                if(isSame(str,i,j)) {
+                    ans = Math.max(ans,j-i);
+                    System.out.println(j);
+                }
             }
         }
 
-        Set<String> set = new HashSet<>();
-        set.add("java");
-        set.add("java");
-        set.add("python");
-        System.out.println(set.contains("java"));
-        return null;
+        return ans;
     }
 
-    private boolean isSame(String s,int start,int end){
+    private static boolean isSame(String s,int start,int end){
         Set<Character> set = new HashSet();
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = start; i < end; i++) {
             Character c = s.charAt(i);
             if (set.contains(c)){
                 return false;
             }
+            set.add(c);
         }
         return true;
     }
